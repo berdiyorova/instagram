30880bb136480986202dc20c48ac68a8787af267
berdiyorova <rano.baxromovna@gmail.com>
2024-12-01 05:19:57 +0500

    some tests are writed

--- posts/urls.py
+++ posts/urls.py
@@ -1,13 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from posts import views
 
 router = DefaultRouter()
-router.register(prefix=r'', viewset=views.PostViewSet)
+router.register(prefix=r'posts', viewset=views.PostViewSet, basename='posts')
 router.register(prefix=r'comments', viewset=views.CommentViewSet)
 
 urlpatterns = [
     path('like/', views.PostLikeView.as_view()),
     path('comments/like/', views.CommentLikeView.as_view()),
-] + router.urls
+    path('', include(router.urls)),
+]
